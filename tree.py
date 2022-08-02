221bd7a41e43338a81b148931f03d0fd325e4547
-Z- <iso25010@seznam.cz>
2022-08-02 15:33:31 +0200

    tree1

--- tree.py
+++ tree.py
@@ -2,4 +2,12 @@ def tree(aString):
     if aString:
         tree(aString[:-1])
         print(aString)
+
+def tree1(aChar,aNumber):
+    for x in range(aNumber):
+        print(aChar*x)
+
+
+
 tree('*****')
+tree1('*',6)
