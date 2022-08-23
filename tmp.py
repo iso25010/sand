2eb886818dd469b75057342f50e069775f0aacb5
-Z- <iso250010@seznam.cz>
2022-08-23 20:33:01 +0200

    cistka na vnitru

--- tmp.py
+++
@@ -1,5 +0,0 @@
-l=[[i + j*10 for i in range(10)] for j in range(10)]
-
-for x in range(len(l)):
-    print([i for i in l[x] if i<3])
-
--- tree.py
+++ tree.py
@@ -3,15 +3,8 @@ def tree(aString):
         tree(aString[:-1])
         print(aString)
 
-def tree1(aChar,aNumber):
-    aParagraph=''
-    for x in range(aNumber):
-        aParagraph += (aChar*x) +'\n'
-    return aParagraph
+
 
 
 
 tree('*****')
-aText=tree1('*',6)
-
-print(aText)
