ccca74938891bbf8d5108105cbab8e8998434b99
-Z- <iso250010@seznam.cz>
2022-10-19 19:31:01 +0200

    Hand writing Binary search

--- asciiArt.py
+++ asciiArt.py
@@ -4,4 +4,12 @@ import pywhatkit
 g=glob.glob('*.jpg')
 for f in g:
     pywhatkit.image_to_ascii_art(f,f[:-4])
+    s=open(f[:-4]+'.txt',mode='r')
+    print(s.read())
+    s.close()
+
+with open('doc.txt',mode='r',encoding='utf-8') as s:
+    str=s.read()
+    pywhatkit.text_to_handwriting(str,"doc.png")
+
 
