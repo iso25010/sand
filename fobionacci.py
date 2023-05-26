bf784ebceff8f8503f04fbd446e722d5cbcec9ff
Boháč Zdeněk <zdenek.bohac@o2.cz>
2023-05-26 17:14:04 +0200

    Fibonacci sequence

---
+++ fobionacci.py
@@ -0,0 +1,14 @@
+def fob(f,fn=[0,1]):
+    if f==1:
+        print ('F({0}) -> {1}'.format(len(fn)-1,fn))
+        return 
+    if f==0:
+        print([0])
+        return 
+    if f<0:
+        return
+    fn.append(fn[-1]+fn[-2])
+    #print (fn)
+    fob(f-1,fn)
+
+fob(17)
